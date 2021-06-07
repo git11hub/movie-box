3fd8eeee1821cfa6922fce5feb2473750b823e67
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-06-07 15:02:05 +0600

    dynamic banner completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import requests from './requests';
 import Row from './Row';
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="app">
+      {/* Nav */}
+      <Banner />
       <h1>lets build movie box</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row 
+      title="NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
--- src/Row.js
+++ src/Row.js
@@ -1,42 +1,45 @@
-import axios from './axios';
-import React, { useState, useEffect } from 'react';
-import './Row.css';
+import axios from "./axios";
+import React, { useState, useEffect } from "react";
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    // A snippet of code which runs based on a specific condition/variable
-    useEffect(() => {
-        // if [], run once when the row loads, and don't run again
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    console.log(movies);
+  // console.log(movies);
 
-    return (
-        <div className="row">
-            <h2>{title}</h2>
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            <div className="row_posters">
-                {/* several row_poster(s) */}
+      <div className="row_posters">
+        {/* several row_poster(s) */}
 
-                {movies.map(movie => (
-                    <img
-                        key={movie.id}
-                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-                ))}
-            </div>
-
-        </div>
-    )
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
